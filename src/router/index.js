fdce36ac6de65b1a3da4079a49c60ef0c218bf0e
[MARITH-F] <[1974681842@qq.com]>
2019-09-13 11:39:12 +0800

    config router lazy

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,25 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+//配置路由懒加载时,无需import
+// import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',  //去掉路径中的/#/
   routes: [
-    {
+    { //路由重定向
+      path: '/',
+      redirect(){
+        return '/HelloWorld'
+      }
+    },
+    { //首页面
       path: '/',
       name: 'HelloWorld',
-      component: HelloWorld
+      // component: HelloWorld
+      // 配置路由懒加载的写法
+      component:reslove => (require(["@/components/HelloWorld"],reslove))
     }
   ]
 })
