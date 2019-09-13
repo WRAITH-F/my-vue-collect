4b38a4b54cd04c868344e23473020e56eaca0eeb
[MARITH-F] <[1974681842@qq.com]>
2019-09-13 13:06:12 +0800

    修改路由配置-懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -6,16 +6,18 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',  //去掉路径中的/#/
+  //去掉链接中的/#/
+  mode: 'history',
   routes: [
-    { //路由重定向
+    {
       path: '/',
+      name: 'HelloWorld',
       redirect(){
         return '/HelloWorld'
       }
     },
-    { //首页面
-      path: '/',
+    {
+      path: '/HelloWorld',
       name: 'HelloWorld',
       // component: HelloWorld
       // 配置路由懒加载的写法
