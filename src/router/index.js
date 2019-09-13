fa16cc4287731ad5de7aa5f2aef3d6f891796de1
[MARITH-F] <[1974681842@qq.com]>
2019-09-13 13:23:15 +0800

    合并分支

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,27 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+//配置路由懒加载时,无需import
+// import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
 export default new Router({
+  //去掉链接中的/#/
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
-      component: HelloWorld
+      redirect(){
+        return '/HelloWorld'
+      }
+    },
+    {
+      path: '/HelloWorld',
+      name: 'HelloWorld',
+      // component: HelloWorld
+      // 配置路由懒加载的写法
+      component:reslove => (require(["@/components/HelloWorld"],reslove))
     }
   ]
 })
